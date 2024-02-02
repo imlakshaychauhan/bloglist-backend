2e0aac821ca9de704f7d7725abed57b70ce20e64
Lakshay Chauhan <imlakshaychauhan@gmail.com>
2024-02-02 17:55:55 +0530

    Middlewares Done

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const mongoose = require("mongoose");
 const Blog = require("./models/blog");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -19,17 +21,25 @@ mongoose
   });
 
 app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+  Blog.find({})
+    .then((blogs) => {
+      response.json(blogs);
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/blogs", (request, response) => {
   const blog = new Blog(request.body);
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
 });
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 const PORT = config.PORT || 3003;
 app.listen(PORT, () => {
